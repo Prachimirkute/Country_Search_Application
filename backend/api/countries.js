4fba1d1f86790a1eb93cad4fae283a59a6e530c9
Prachimirkute <prachimirkute27@gmail.com>
2023-11-26 21:54:24 +0000

    backend country api added

--- backend/api/countries.js
+++ backend/api/countries.js
@@ -2,13 +2,18 @@ const express = require('express');
 const axios = require('axios');
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.get('/:country', async (req, res) => {
+    const country = req.params.country;
     try {
-      const response = await axios.get('https://restcountries.com/v3.1/all');
-     res.json(response.data);
+      const response = await axios.get(`https://restcountries.com/v3.1/name/${country}?fullText=true`);
+      debugger;
+      const countryInfo = response.data[0];
+       res.json(countryInfo);
+
     } catch (error) {
       console.error('Error fetching country information', error);
       res.status(500).json({ error: 'Internal Server Error' });
     }
+    
   });
   module.exports = router;
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const countryList = require('./api/countryList');
+const countries = require('./api/countries');
 const app = express();
 const axios = require('axios');
 const cors = require('cors');
@@ -7,24 +8,25 @@ const PORT = 3001;
 
 app.use(cors()); 
 app.use("/api/countryList", countryList);
+app.use("/api/countries", countries);
 // app.get('/',  (req, res) => {
 // res.send("server start");
 // });
 
-app.get('/countries/:country', async (req, res) => {
-  const country = req.params.country;
-  try {
-    const response = await axios.get(`https://restcountries.com/v3.1/name/${country}?fullText=true`);
-    debugger;
-    const countryInfo = response.data[0];
-    const result = res.json(countryInfo);
-    res.send(result);
-  } catch (error) {
-    console.error('Error fetching country information', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+// app.get('/countries/:country', async (req, res) => {
+//   const country = req.params.country;
+//   try {
+//     const response = await axios.get(`https://restcountries.com/v3.1/name/${country}?fullText=true`);
+//     debugger;
+//     const countryInfo = response.data[0];
+//     const result = res.json(countryInfo);
+//     res.send(result);
+//   } catch (error) {
+//     console.error('Error fetching country information', error);
+//     res.status(500).json({ error: 'Internal Server Error' });
+//   }
   
-});
+// });
 // app.get('/api/countryList', async (req, res) => {
 //   try {
 //     const response = await axios.get('https://restcountries.com/v3.1/all');
