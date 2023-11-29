66446c7f61fa202683f63d7fbbbba78cf002f2e0
Prachimirkute <125886153+Prachimirkute@users.noreply.github.com>
2023-11-29 00:37:34 +0000

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,34 +9,6 @@ const PORT = 3001;
 app.use(cors()); 
 app.use("/api/countryList", countryList);
 app.use("/api/countries", countries);
-// app.get('/',  (req, res) => {
-// res.send("server start");
-// });
-
-// app.get('/countries/:country', async (req, res) => {
-//   const country = req.params.country;
-//   try {
-//     const response = await axios.get(`https://restcountries.com/v3.1/name/${country}?fullText=true`);
-//     debugger;
-//     const countryInfo = response.data[0];
-//     const result = res.json(countryInfo);
-//     res.send(result);
-//   } catch (error) {
-//     console.error('Error fetching country information', error);
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
-  
-// });
-// app.get('/api/countryList', async (req, res) => {
-//   try {
-//     const response = await axios.get('https://restcountries.com/v3.1/all');
-//     const result = res.json(response.data);
-//     res.send(result);
-//   } catch (error) {
-//     console.error('Error fetching country information', error);
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// });
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
